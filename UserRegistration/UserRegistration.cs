5e9aeacd81bec59eb07ea583e91393147dff1aca
Mahesh Kangude <Mahesh Kangude@DESKTOP-DF3BBMR>
2021-02-05 21:54:12 +0530

    [MaheshKangude] ADDED: SampleEmails Validation

--- UserRegistration/UserRegistration.cs
+++ UserRegistration/UserRegistration.cs
@@ -12,12 +12,26 @@ namespace UserRegistration
             string EmailAddress = "maheshkangude@gmail.com";
             string MobileNumber = "91 8483996231";
             string Password = "sdnc*Kjn8dc";
+            
+            string[] EmailList = {"abc@yahoo.com","abc-100@yahoo.com",  "abc.100@yahoo.com",
+                                "abc111@abc.com",  "abc-100@abc.net",  "abc.100@abc.com.au",  "abc@1.com",
+                                "abc@gmail.com.com",  "abc+100@gmail.com",
+                                "abc",   "abc@.com.my",  "abc123@gmail.a",  "abc123@.com",
+                                "abc123@.com.com",  ".abc@abc.com",  "abc()*@gmail.com",  "abc@%*.com",
+                                "abc..2002@gmail.com",  "abc.@gmail.com",  "abc@abc@gmail.com",
+                                "abc@gmail.com.1a",  "abc@gmail.com.aa.au"};
 
             userRegexTest.ValidateFirstName(FirstName);
             userRegexTest.ValidateLastName(LastName);
             userRegexTest.ValidateEmailAddress(EmailAddress);
             userRegexTest.ValidateMobileNumber(MobileNumber);
             userRegexTest.ValidatePassword(Password);
+
+            foreach (string SampleEmailAddress in EmailList)
+            {
+                userRegexTest.ValidateEmailAddress(SampleEmailAddress);
+
+            }
         }
     }
 }

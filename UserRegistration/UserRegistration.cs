4f35594a7c37a4e8273021a5de30708845261c7b
Mahesh Kangude <Mahesh Kangude@DESKTOP-DF3BBMR>
2021-02-05 19:33:58 +0530

    Add project files.

---
+++ UserRegistration/UserRegistration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class UserRegistration
+    {
+        static void Main(string[] args)
+        {
+            UserRegexTest userRegexTest = new UserRegexTest();
+        }
+    }
+}
